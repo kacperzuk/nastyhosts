84151071f9ad0cec24c5ee6b109b874903e6c142
Kacper Żuk <kacper.b.zuk@gmail.com>
2015-10-17 22:17:06 +0200

    Make eslint happy

--- bin/nastyhosts.js
+++ bin/nastyhosts.js
@@ -12,40 +12,40 @@ const networksPath = path.resolve("evil-networks.json");
 
 
 if(argv.init) {
-  fs.access(configPath, function(err) {
+  fs.access(configPath, (err) => {
     if(err) {
-      console.log("Creating " + configPath);
+      console.log(`Creating ${configPath}`);
       fs.writeFile(configPath, JSON.stringify(require("../lib/config.js").defaultConfig, null, 2));
     }
   });
 
-  fs.access(networksPath, function(err) {
+  fs.access(networksPath, (err) => {
     if(err) {
-      console.log("Creating " + networksPath);
+      console.log(`Creating ${networksPath}`);
       fs.writeFile(networksPath, JSON.stringify(["127.0.0.0/8"], null, 2));
     }
   });
 
-  fs.access(hostnamesPath, function(err) {
+  fs.access(hostnamesPath, (err) => {
     if(err) {
-      console.log("Creating " + hostnamesPath);
+      console.log(`Creating ${hostnamesPath}`);
       fs.writeFile(hostnamesPath, JSON.stringify([], null, 2));
     }
   });
 
 } else {
-  [configPath, hostnamesPath, networksPath].forEach(function(v) {
+  [configPath, hostnamesPath, networksPath].forEach((v) => {
     try {
       fs.accessSync(v);
     } catch(err) {
-      console.log("Missing " + v);
+      console.log(`Missing ${v}`);
       console.log("Have you run --init?");
       process.exit(1);
     }
   });
-  require("../lib/config.js")(function(config) {
-    const plugins = config.plugins.map(function(plugin) {
-      return require("nastyhosts-" + plugin + "-plugin");
+  require("../lib/config.js")((config) => {
+    const plugins = config.plugins.map((plugin) => {
+      return require(`nastyhosts-${plugin}-plugin`);
     });
     require("../lib/server.js")(config, plugins);
   });
